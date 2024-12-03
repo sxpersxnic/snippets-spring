7e093a127bea77e167f5b7ddd72a670ba42ecf78
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-12-03 10:31:25 +0000

    Init spring

---
+++ src/test/java/com/snippets/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippets.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
